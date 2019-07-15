05853874d8fc826e293354e8be2ee97043332141
Diego Rodriguez <diego.rodriguez@cern.ch>
2019-07-15 15:07:36 +0200

    config: more granular DB configuration

--- reana_db/config.py
+++ reana_db/config.py
@@ -10,7 +10,27 @@
 
 import os
 
+DB_NAME = os.getenv('REANA_DB_NAME', 'reana')
+"""Database name."""
+
+DB_USERNAME = os.getenv('REANA_DB_USERNAME', 'reana')
+"""Database user name."""
+
+DB_PASSWORD = os.getenv('REANA_DB_PASSWORD', 'reana')
+"""Database password."""
+
+DB_HOST = os.getenv('REANA_DB_HOST', 'db')
+"""Database service host."""
+
+DB_PORT = os.getenv('REANA_DB_PORT', '5432')
+"""Database service port."""
+
 SQLALCHEMY_DATABASE_URI = \
-    os.getenv('REANA_SQLALCHEMY_DATABASE_URI',
-              'postgresql+psycopg2://reana:reana@db:5432/reana')
+    os.getenv(
+        'REANA_SQLALCHEMY_DATABASE_URI',
+        'postgresql+psycopg2://{username}:{password}'
+        '@{host}:{port}/{db}'.format(
+            username=DB_USERNAME, password=DB_PASSWORD, host=DB_HOST,
+            port=DB_PORT, db=DB_NAME
+        ))
 """SQLAlchemy database location."""
