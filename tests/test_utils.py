0fb758a18fe68bb983052eba70948a4dc423cb37
Tibor Šimko <tibor.simko@cern.ch>
2019-12-19 17:04:29 +0100

    tests: initial skeleton for tests
    * Plugs first simple test and activates code coverage for this package.
    
    * Post-release version bump.
    
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2018 CERN.
+# Copyright (C) 2019 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""REANA-DB."""
+"""REANA-DB utils tests."""
 
 from __future__ import absolute_import, print_function
 
-from .version import __version__
 
-__all__ = ('__version__', )
+def test_build_workspace_path():
+    """Tests for build_workspace_path()."""
+    from reana_db.utils import build_workspace_path
+    assert build_workspace_path(0) == 'users/0/workflows'
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2018 CERN.
+# Copyright (C) 2018, 2019 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""REANA-DB."""
+"""REANA-DB tests."""
 
 from __future__ import absolute_import, print_function
 
-from .version import __version__
 
-__all__ = ('__version__', )
+def test_version():
+    """Test version import."""
+    from reana_db import __version__
+    assert __version__
