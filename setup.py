49a8d12c304f837a39351614140ce3893c656315
Camila Diaz <marcdiazsan@unal.edu.co>
2021-08-09 11:05:16 -0500

    release: 0.8.0a20

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ readme = open("README.rst").read()
 history = open("CHANGES.rst").read()
 
 tests_require = [
-    "pytest-reana>=0.8.0a5,<0.9.0",
+    "pytest-reana>=0.8.0a6,<0.9.0",
 ]
 
 extras_require = {
@@ -43,7 +43,7 @@ install_requires = [
     "SQLAlchemy>=1.2.7,<1.4.0",
     "sqlalchemy-utils>=0.35.0",
     "cryptography>=2.9.2",  # Required by sqlalchemy_utils.EncryptedType
-    "reana-commons>=0.8.0a19,<0.9.0",
+    "reana-commons>=0.8.0a21,<0.9.0",
 ]
 
 packages = find_packages()
