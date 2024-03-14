ec7c5e5801fb7954bc8b8e652674e64d67526838
Tibor Šimko <tibor.simko@cern.ch>
2024-03-14 14:31:26 +0100

    chore(master): release 0.95.0a1
    Release-As: 0.95.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2018, 2019, 2020, 2021, 2022, 2023 CERN.
+# Copyright (C) 2018, 2019, 2020, 2021, 2022, 2023, 2024 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -19,7 +19,7 @@ readme = open("README.md").read()
 history = open("CHANGELOG.md").read()
 
 tests_require = [
-    "pytest-reana>=0.9.2,<0.10.0",
+    "pytest-reana>=0.95.0a1,<0.96.0",
 ]
 
 extras_require = {
@@ -47,7 +47,7 @@ install_requires = [
     "psycopg2-binary>=2.6.1",
     "SQLAlchemy>=1.2.7,<1.4.0",
     "SQLAlchemy-Utils[encrypted]>=0.36.8",
-    "reana-commons>=0.9.4,<0.10.0",
+    "reana-commons>=0.9.4,<0.96.0",
 ]
 
 packages = find_packages()
